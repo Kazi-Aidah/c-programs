170a761bf5cffac90de76f4663740d5aca98259f
Kazi Aidah Haque <kaziaidah19@gmail.com>
2026-06-20 09:50:28 +0600

    Create try/uppercase-lowercase.c

---
+++ try/uppercase-lowercase.c
@@ -0,0 +1,19 @@
+// input lowercase, get uppercase & vice versa
+#include<stdio.h>
+#include<ctype.h>
+int main() {
+	char c;
+	
+	scanf("%c", &c);
+	if(isupper(c)) {
+		c = tolower(c);
+	} 
+	
+	else(islower(c)); {
+		c = toupper(c);
+	}
+	
+	printf("%c", c);
+	
+	return 0;
+}
