4a30d75a7e5c1c0da1ebeabab96cfeaff54009bb
Kazi Aidah Haque <kaziaidah19@gmail.com>
2026-06-21 10:42:54 +0600

    Create 14_ascii_of_alphabet.c

---
+++ 14_ascii_of_alphabet.c
@@ -0,0 +1,10 @@
+// 14. Print ASCII value of alphabet
+#include<stdio.h>
+int main() {
+    char c;
+    printf("Enter an alphabet: ");
+    scanf("%c", &c);
+    printf("%d", c); // %d = integer | get integer of c
+    
+    return 0;
+}
