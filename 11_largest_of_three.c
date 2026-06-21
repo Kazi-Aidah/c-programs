9a0e8ca3667119a57151d9225e6912d1ccb53872
Kazi Aidah Haque <kaziaidah19@gmail.com>
2026-06-21 10:15:55 +0600

    Create 11_largest_of_three.c

---
+++ 11_largest_of_three.c
@@ -0,0 +1,13 @@
+// 11. Find the largest among three numbers
+#include<stdio.h>
+int main() {
+    int a, b, c;
+    printf("Input three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if(a > b && a > c) { printf("%d is the largest number", a); }
+    else if(b > a && b > c) { printf("%d is the largest number", b); }
+    else { printf("%d is the largest number", c); }
+    
+    return 0;
+}
