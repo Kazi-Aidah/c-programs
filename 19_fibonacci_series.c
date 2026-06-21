ca3c46fbe586cf33d13593cbad27bbdec909f82d
Kazi Aidah Haque <kaziaidah19@gmail.com>
2026-06-21 13:06:51 +0600

    Create 19_fibonacci_series.c

---
+++ 19_fibonacci_series.c
@@ -0,0 +1,17 @@
+// 19. Fibonacci Series
+
+#include <stdio.h>
+int main() {
+    int n, a = 0, b = 1, next;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("\n");
+    return 0;
+}
